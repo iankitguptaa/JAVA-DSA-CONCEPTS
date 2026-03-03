50c7f60917a2f294a37e121ccf8e68392bde1415
Ankit Gupta <iankitgupta04@gmail.com>
2026-03-03 17:38:24 +0530

    fix

--- BinarySearchRecursion.java
+++ BinarySearchRecursion.java
@@ -1,7 +1,7 @@
 public class BinarySearchRecursion {
     public static void main(String[] args) {
         int[] arr = { 2, 5, 7, 9, 10, 12, 14 };
-        int target = 14;
+        int target = 5;
         System.out.println(binarySearch(arr, target, 0, arr.length - 1));
     }
 
@@ -10,12 +10,12 @@ public class BinarySearchRecursion {
             return -1;
         }
         int mid = start + (end - start) / 2;
-        if (target == arr[mid]) {
-            return mid;
-        }
         if (target < arr[mid]) {
             return binarySearch(arr, target, start, mid - 1);
         }
-        return binarySearch(arr, target, mid + 1, end);
+        if (target > arr[mid]) {
+            return binarySearch(arr, target, mid + 1, end);
+        }
+        return mid;
     }
 }
