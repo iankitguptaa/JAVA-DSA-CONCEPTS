db767050e5cfde3703528f29d9fd1be4a929014b
Ankit Gupta <iankitgupta04@gmail.com>
2026-07-02 16:45:06 +0530

    fix

--- BubbleSortAlgo.java
+++ BubbleSortAlgo.java
@@ -1,19 +1,17 @@
 import java.util.Arrays;
-public class BubbleSortAlgorithm {
+
+public class BubbleSortAlgo {
     public static void main(String[] args) {
-        int[] arr = { 5, 4, 4, 3, 3, 2, 1, 1, 0 };
+        int[] arr = { 6, 2, 1, 9, 10, 23, 42, 98, 67 };
         bubbleSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
     static void bubbleSort(int[] arr) {
         boolean swapped;
-        // run the step n-1 times
         for (int i = 0; i < arr.length; i++) {
             swapped = false;
-            // for each step, max item will come at the last respectice index
-            for (int j = 1; j < arr.length - i; j++) {
-                // swap if the item is smaller than the previous item
+            for (int j = 1; j < arr.length - 1; j++) {
                 if (arr[j] < arr[j - 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j - 1];
@@ -21,7 +19,7 @@ public class BubbleSortAlgorithm {
                     swapped = true;
                 }
             }
-            if (!swapped) { // !false = true
+            if (!swapped) {
                 break;
             }
         }
