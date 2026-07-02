0bc0fff500a5cf181b72c2b205dd32d2a06ae8b3
Ankit Gupta <iankitgupta04@gmail.com>
2026-07-02 17:17:09 +0530

    fix

---
+++ SelectionSortAlgo.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class SelectionSortAlgo {
+    public static void main(String[] args) {
+        int[] arr = { 7, 8, 3, 1, 9, 2, 0, 10, 15 };
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int last = arr.length - i - 1;
+            int maxIndex = getMaxIndex(arr, 0, last);
+            swap(arr, maxIndex, last);
+        }
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    private static int getMaxIndex(int[] arr, int start, int end) {
+        int max = start;
+        for (int i = start; i <= end; i++) {
+            if (arr[max] < arr[i]) {
+                max = i;
+            }
+        }
+        return max;
+    }
+}
