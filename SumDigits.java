724f4b478383e2e85fb7cae1683073f524209e16
Ankit Gupta <iankitgupta04@gmail.com>
2026-07-06 16:22:56 +0530

    fix

--- SumDigits.java
+++ SumDigits.java
@@ -5,8 +5,8 @@ public class SumDigits {
     }
 
     static int sum(int n) {
-        if (n == 0) {
-            return 0;
+        if (n % 10 == 0) {
+            return n;
         }
         return (n % 10) + sum(n / 10);
     }
