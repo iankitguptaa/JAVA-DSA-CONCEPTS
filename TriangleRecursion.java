aab727174910a2683de4f0a7fcb5779162298583
Ankit Gupta <iankitgupta04@gmail.com>
2026-07-12 01:39:14 +0530

    fix

--- TriangleRecursion.java
+++ TriangleRecursion.java
@@ -1,6 +1,7 @@
 public class TriangleRecursion {
     public static void main(String[] args) {
         Triangle(4, 0);
+        Triangle2(4, 0);
     }
 
     static void Triangle(int r, int c) {
@@ -13,6 +14,19 @@ public class TriangleRecursion {
         } else {
             System.out.println();
             Triangle(r - 1, 0);
+        }
+    }
+
+    static void Triangle2(int r, int c) {
+        if (r == 0) {
+            return;
+        }
+        if (c < r) {
+            Triangle2(r, c + 1);
+            System.out.print("*");
+        } else {
+            Triangle2(r - 1, 0);
+            System.out.println();
         }
     }
 }
